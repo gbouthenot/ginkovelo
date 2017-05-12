53092a1ce89ecfc8538f8c6cc8b7768225f7502d
Gilles Bouthenot <gbouthenot@bitbucket.org>
2017-05-12 15:30:33 +0200

    eslint

--- page.js
+++ page.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-plusplus, no-console, no-multiple-empty-lines, no-param-reassign,
+   class-methods-use-this */
 class Ginko {
   constructor(idReq, urlNomStation) {
     this.nbReq = 0;
@@ -7,64 +9,64 @@ class Ginko {
     this.dom = document.getElementById(idReq);
   }
 
-  timeIso(date) {
-    if (!date) { date = new Date(); }
-    return ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)
-      + ":" + ("0" + date.getSeconds()).slice(-2);
+  timeIso(date = new Date()) {
+    /* eslint-disable prefer-template */
+    return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
+      + ':' + ('0' + date.getSeconds()).slice(-2);
+    /* eslint-enable prefer-template */
   }
-
 
   replaceAll(string, search, replace) {
-    return string.split(search).join(replace)
+    return string.split(search).join(replace);
   }
 
-  promFetch () {
+  promFetch() {
     const nbReq = ++this.nbReq;
-    const nbReqDom = this.dom.querySelector(".nbReq")
-    nbReqDom.innerHTML = nbReq + "…";
+    const nbReqDom = this.dom.querySelector('.nbReq');
+    nbReqDom.innerHTML = `${nbReq}…`;
     return fetch(this.url)
-    .then((r)=>{nbReqDom.innerHTML = `${nbReq} (${this.timeIso()})`; return r.json();})
+    .then((r) => { nbReqDom.innerHTML = `${nbReq} (${this.timeIso()})`; return r.json(); });
   }
 
-  fetch () {
+  fetch() {
     const prom = this.promFetch();
 
     if (!this.rendered) {
-      prom.then(data=>this.firstReq(data))
+      prom.then(data => this.firstReq(data));
       this.rendered++;
     }
-    prom.then(data=>this.nextReq(data))
-    prom.catch(r=>console.log("Erreur", r))
+    prom.then(data => this.nextReq(data));
+    prom.catch(r => console.log('Error', r));
   }
 
   firstReq(data) {
-    const tpldest = document.getElementById("tplDestination").innerHTML;
+    const tpldest = document.getElementById('tplDestination').innerHTML;
 
     // nom de la station
-    this.dom.querySelector(".nomArret .value").innerHTML = data.objets.nomExact;
+    this.dom.querySelector('.nomArret .value').innerHTML = data.objets.nomExact;
 
     // ne garde que les lignes de tram
-    let r = data.objets.listeTemps.filter(a=>["101", "102"].includes(a.idLigne));
+    let r = data.objets.listeTemps.filter(a => ['101', '102'].includes(a.idLigne));
 
     // ne garde que le nom de destination puis unique
-    r = r.map(a=>a.destination).filter((x, i, a) => a.indexOf(x) == i);
+    r = r.map(a => a.destination).filter((x, i, a) => a.indexOf(x) === i);
 
     // pour chaque destination
-    r.forEach((nomdest)=>{
-      let tpl = this.replaceAll(tpldest, "${NOMDEST}", nomdest);
-      this.dom.querySelector(".destinations").innerHTML += tpl;
+    r.forEach((nomdest) => {
+      const tpl = this.replaceAll(tpldest, '#{NOMDEST}', nomdest);
+      this.dom.querySelector('.destinations').innerHTML += tpl;
     });
 
     return data;
   }
 
   nextReq(r) {
-    this.dom.querySelectorAll("ul li.destination").forEach( (domdest) => {
-      const domdestvalue = domdest.getAttribute("data-nomdest");
+    this.dom.querySelectorAll('ul li.destination').forEach((domdest) => {
+      const domdestvalue = domdest.getAttribute('data-nomdest');
       let temps = r.objets.listeTemps;
-      temps = temps.filter((a)=>a.destination==domdestvalue&&a.temps[0]!='I');
-      temps = temps.map((a)=>parseInt(a.temps,10)).sort((a,b)=>a-b);
-      domdest.querySelector(".horaires").innerHTML = temps;
+      temps = temps.filter(a => a.destination === domdestvalue && a.temps[0] !== 'I');
+      temps = temps.map(a => parseInt(a.temps, 10)).sort((a, b) => a - b);
+      domdest.querySelector('.horaires').innerHTML = temps;
     });
   }
 }
@@ -83,35 +85,36 @@ class Velocite {
     this.dom = document.getElementById(idReq);
   }
 
-  timeIso(date) {
-    if (!date) { date = new Date(); }
-    return ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)
-      + ":" + ("0" + date.getSeconds()).slice(-2);
+  timeIso(date = new Date()) {
+    /* eslint-disable prefer-template */
+    return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
+      + ':' + ('0' + date.getSeconds()).slice(-2);
+    /* eslint-enable prefer-template */
   }
 
-  promFetch () {
+  promFetch() {
     const nbReq = ++this.nbReq;
-    const nbReqDom = this.dom.querySelector(".nbReq")
-    nbReqDom.innerHTML = nbReq + "…";
+    const nbReqDom = this.dom.querySelector('.nbReq');
+    nbReqDom.innerHTML = `${nbReq}…`;
     return fetch(this.url)
-    .then((r)=>{nbReqDom.innerHTML = `${nbReq} (${this.timeIso()})`; return r.json();})
+    .then((r) => { nbReqDom.innerHTML = `${nbReq} (${this.timeIso()})`; return r.json(); });
   }
 
-  fetch () {
+  fetch() {
     const prom = this.promFetch();
 
-    prom.then(data=>{this.nextReq(data)})
-    prom.catch(r=>console.log("Erreur", r))
+    prom.then(data => this.nextReq(data));
+    prom.catch(r => console.log('Erreur', r));
   }
 
   nextReq(r) {
-    let txt = "";
+    let txt = '';
     // trie par numéro
-    r = r.sort((a,b)=>a.number-b.number);
+    r = r.sort((a, b) => a.number - b.number);
 
-    r.forEach(st=>{
+    r.forEach((st) => {
       txt += `${st.name.toLocaleLowerCase()}: ${st.available_bikes}/${st.bike_stands}<br />`;
     });
-    this.dom.querySelector(".stations").innerHTML = txt;
+    this.dom.querySelector('.stations').innerHTML = txt;
   }
 }
