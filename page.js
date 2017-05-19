f4fb900fe726c7fd9be103711e88294bbc13a29c
Gilles Bouthenot <gbouthenot@bitbucket.org>
2017-05-19 16:34:07 +0200

    factorisation

--- page.js
+++ page.js
@@ -4,16 +4,83 @@
 // TODO: add timeout
 // TODO: ginko: change should abort fetch
 
-class Ginko {
-  constructor(idReq, nomStation) {
+class Common {
+  constructor(idReq) {
     this.nbReq = 0;
     this.rendered = 0;
     this.idReq = idReq;
     this.dom = document.getElementById(idReq);
     this.busy = false;
     this.lastUpdate = 0;
+    this.lastData = [];
     this.now = 0;
-    // render stations:
+
+    // Appui sur bouton
+    this.dom.querySelector('.nbReq').addEventListener('click', (e) => {
+      this.fetch();
+    });
+
+    window.setInterval(_ => this.interrupt(), 1000);
+  }
+
+  interrupt() {
+    this.now = Math.floor(Date.now() / 1000);
+    const interval = parseInt(this.dom.querySelector('.interval').value, 10);
+    if (!this.busy && interval > 0 && this.now - this.lastUpdate >= interval) {
+      this.fetch();
+    }
+  }
+
+  timeIso(date = new Date()) {
+    return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
+      + ':' + ('0' + date.getSeconds()).slice(-2);
+  }
+
+  delay(ms) {
+    return value => new Promise(resolve => setTimeout(_ => resolve(value), ms));
+  }
+
+  replaceAll(string, search, replace) {
+    return string.split(search).join(replace);
+  }
+
+  fetch() {
+    this.promFetch()
+    .then((data) => {
+      this.lastData = data;
+      if (!this.rendered) {
+        this.rendered++;
+        this.firstReq(data);
+      }
+      return this.render(data);
+    })
+    .catch(r => console.log('Error', r));
+  }
+
+  promFetch() {
+    const nbReq = ++this.nbReq;
+    const nbReqDom = this.dom.querySelector('.nbReq');
+    this.busy = true;
+    nbReqDom.innerHTML = `${nbReq}…`;
+    return fetch(this.geturl())
+    .then((r) => {
+      this.busy = false;
+      this.lastUpdate = Math.floor(Date.now() / 1000);
+      nbReqDom.innerHTML = `${nbReq} (${this.timeIso()})`;
+      return r.json();
+    });
+  }
+
+  firstReq() {}
+}
+
+
+
+class Ginko extends Common {
+  constructor(idReq, nomStation) {
+    super(idReq);
+
+    // render stations
     let stations = 'Hauts du Chazal; UFR Médecine Pharma; Pôle Santé; CHRU Minjoz; Ile de France; ' +
       'Epoisses; Allende; Micropolis; Malcombe; Rosemont; Brulard; Polygone; Chamars; Canot; Battant; ' +
       'Révolution; République; Parc Micaud; Fontaine Argent; Tristan Bernard; Brûlefoin; Les Vaîtes; ' +
@@ -31,64 +98,11 @@ class Ginko {
       this.rendered = 0;
       this.fetch();
     });
-
-    // Appui sur bouton
-    this.dom.querySelector('.nbReq').addEventListener('click', (e) => {
-      this.fetch();
-    });
-
-    window.setInterval(_ => this.interrupt(), 1000);
   }
 
-  interrupt() {
-    this.now = Math.floor(Date.now() / 1000);
-    const interval = parseInt(this.dom.querySelector('.interval').value, 10);
-    if (!this.busy && interval > 0 && this.now - this.lastUpdate >= interval) {
-      this.fetch();
-    }
-  }
-
-  timeIso(date = new Date()) {
-    return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
-      + ':' + ('0' + date.getSeconds()).slice(-2);
-  }
-
-  replaceAll(string, search, replace) {
-    return string.split(search).join(replace);
-  }
-
-  delay(ms) {
-    return value => new Promise(resolve => setTimeout(_ => resolve(value), ms));
-  }
-
-  promFetch() {
-    const nbReq = ++this.nbReq;
-    const nbReqDom = this.dom.querySelector('.nbReq');
+  geturl() {
     const nomStation = this.dom.querySelector('select').value;
-    const url = `https://www.ginkoopenapi.fr/TR/getTempsLieu.do?nom=${nomStation}`;
-    this.busy = true;
-    nbReqDom.innerHTML = `${nbReq}…`;
-    return fetch(url)
-    .then((r) => {
-      this.busy = false;
-      this.lastUpdate = Math.floor(Date.now() / 1000);
-      nbReqDom.innerHTML = `${nbReq} (${this.timeIso()})`;
-      return r.json();
-    });
-  }
-
-
-  fetch() {
-    this.promFetch()
-    .then((data) => {
-      if (!this.rendered) {
-        this.rendered++;
-        return this.firstReq(data);
-      }
-      return data;
-    })
-    .then(data => this.nextReq(data))
-    .catch(r => console.log('Error', r));
+    return `https://www.ginkoopenapi.fr/TR/getTempsLieu.do?nom=${nomStation}`;
   }
 
   firstReq(data) {
@@ -101,7 +115,8 @@ class Ginko {
     let r = data.objets.listeTemps.filter(a => ['101', '102'].includes(a.idLigne));
 
     // ne garde que le nom de destination puis unique
-    r = r.map(a => a.destination).filter((x, i, a) => a.indexOf(x) === i);
+    r = r.map(a => a.destination);
+    r = r.filter((x, i, a) => a.indexOf(x) === i);
 
     const eldests = this.dom.querySelector('.destinations');
     eldests.innerHTML = '';
@@ -114,7 +129,7 @@ class Ginko {
     return data;
   }
 
-  nextReq(r) {
+  render(r) {
     this.dom.querySelectorAll('ul li.destination').forEach((domdest) => {
       const domdestvalue = domdest.getAttribute('data-nomdest');
       let temps = r.objets.listeTemps;
@@ -132,71 +147,23 @@ class Ginko {
 
 
 
-class Velocite {
+class Velocite extends Common {
   constructor(idReq, contract, apiKey) {
-    this.nbReq = 0;
-    this.idReq = idReq;
+    super(idReq);
+
     this.url = `https://api.jcdecaux.com/vls/v1/stations?contract=${contract}&apiKey=${apiKey}`;
-    this.dom = document.getElementById(idReq);
-    this.busy = false;
-    this.lastUpdate = 0;
-    this.lastData = [];
-    this.now = 0;
 
     // checkbox "show all"
     this.dom.querySelector('input[type=checkbox]').addEventListener('change', (_) => {
       this.render();
     });
-
-    // Appui sur bouton
-    this.dom.querySelector('.nbReq').addEventListener('click', (e) => {
-      this.fetch();
-    });
-
-    window.setInterval(_ => this.interrupt(), 1000);
   }
 
-  interrupt() {
-    this.now = Math.floor(Date.now() / 1000);
-    const interval = parseInt(this.dom.querySelector('.interval').value, 10);
-    if (!this.busy && interval > 0 && this.now - this.lastUpdate >= interval) {
-      this.fetch();
-    }
+
+  geturl() {
+    return this.url;
   }
 
-  timeIso(date = new Date()) {
-    /* eslint-disable prefer-template */
-    return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
-      + ':' + ('0' + date.getSeconds()).slice(-2);
-    /* eslint-enable prefer-template */
-  }
-
-  delay(ms) {
-    return value => new Promise(resolve => setTimeout(_ => resolve(value), ms));
-  }
-
-  promFetch() {
-    const nbReq = ++this.nbReq;
-    const nbReqDom = this.dom.querySelector('.nbReq');
-    this.busy = true;
-    nbReqDom.innerHTML = `${nbReq}…`;
-    return fetch(this.url)
-    .then((r) => {
-      this.busy = false;
-      this.lastUpdate = Math.floor(Date.now() / 1000);
-      nbReqDom.innerHTML = `${nbReq} (${this.timeIso()})`;
-      return r.json();
-    });
-  }
-
-  fetch() {
-    this.promFetch()
-    .then((data) => {
-      this.lastData = data;
-      this.render();
-    })
-    .catch(r => console.log('Erreur', r));
-  }
 
   render() {
     const r = this.lastData;
@@ -222,7 +189,7 @@ class Velocite {
       const ava = st.available_bikes;
       const tot = st.bike_stands;
       const pc = ava / tot;
-      let cls = '';console.log(pc);
+      let cls = '';
       if (pc >= 0.7) {
         cls = 'verygood';
       } else if (pc < 0.2) {
